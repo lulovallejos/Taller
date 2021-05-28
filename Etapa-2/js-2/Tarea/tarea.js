4bf6fc149ba1054e24e9d548db2fd0f59a29fb25
Lulo Vallejos <val20021@byui.edu>
2021-05-28 16:41:34 -0300

    .

---
+++ Etapa-2/js-2/Tarea/tarea.js
@@ -0,0 +1,121 @@
+  /* 
+    1.analiza el siguiente código y escribe qué se va a ejecutar:
+    se ejecuta la opcion:...
+  */
+    if(true) {
+      console.log("Opción 1");
+    }
+    else {
+      console.log("Opción 2");
+    }
+
+  /* 
+    2.analiza el siguiente código y escribe qué se va a ejecutar:
+    se ejecuta la opcion:... opción 3
+  */
+    var numero1 = 20;
+    var numero2 = 10;
+    var resultado = numero1 + numero2;
+
+    if(resutado < 15) {
+      console.log("Opción 1")
+    }
+    else if(resultado === 25) {
+      console.log("Opción 2")
+    }
+    else {
+      console.log("Opción 3")
+    }
+
+  /* 
+    3.analiza el siguiente código y escribe qué se va a ejecutar:
+    se ejecuta la opcion:... opción 2
+  */
+
+    let alumno = "Alberto";
+
+    if(alumno === "Juan") {
+      console.log("Opción 1")
+    }
+
+    else if(alumno === "Alberto") {
+      console.log("Opción 2")
+    }
+
+    else {
+      console.log("Opción 3")
+    }
+
+  /* 
+    4.Crea un programa que pregunte la edad de una persona, y responderle
+    si tiene edad de irse a una misión o no. Para simplificar puedes elegir la edad
+    del hombre o de la mujer.
+    Pistas: Puede que las condiciones te sirvan de mucho, utiliza variables para guardar lo que consideres.
+    Puedes usar el console.log() para dar el mensaje de resultado.
+  */
+
+
+    var edad = 18; 
+    var sexo = "hombre"; 
+    var estado = "casado"; 
+
+    if(edad >= 18) {
+      console.log("Tenés la edad para salir a la misión")
+    }
+    else if(edad < 18) {
+      console.log("No podés salir.")
+    }
+    else(sexo === hombre); {
+      console.log("Podés salir a la misión.")
+    }
+    
+
+  /* 
+    5.analiza el siguiente código y escribe qué se va a ejecutar:
+    se ejecuta la opcion:... opción 3
+  */
+
+    var nombre = "Daniela";
+    var edad = 52;
+    var miembro = true;
+    
+
+    var mensaje = "";
+
+    if(nombre.length > 5 || edad < 20) {
+      var mensaje = "Acabas de entrar a opcion 1";
+    }
+
+    else if(nombre.length > 7 && edad === 52) {
+      var mensaje = "Acabas de entrar a opcion 2";
+    }
+
+    else if(nombre.length === 7 && edad > 20) {
+      var mensaje = "Acabas de entrar a opcion 3";
+    }
+
+  /* 
+    6.Este código trae errores. Revisalo detenidamente y hazlo funcionar.
+    Funciona?? Ahora.. analiza que devuelve el primer console (debes quitar los //)
+    Ahora, analiza que devuelve el segundo console (para probarlo quita los //)
+    Revisa los consoles de forma separada.
+  */
+    var miNombre = "Camilo";
+
+    for(let i = 0 i < miNombre.lengt; i++) {
+      // console.log(i);
+      // console.log(miNombre[i]);
+    }
+
+  /* 
+    7.Imprime con un console.log los numeros del 1 al 100
+  */
+    for(let i = 1;i <= 100;i++){
+      console.log(i);
+  }
+
+  /* 
+    8.Crea un archivo txt y explica con palabras sencillas qué es un bucle, para qué sirven las condiciones.
+  Explica qué utilidad tienen los operadores logicos.
+  */
+
---
+++ Etapa-2/js-3/Tarea/tarea.js
@@ -0,0 +1,89 @@
+//NOTA: LAs funciones se utilizan para hacer una utilidad con el mismo código.
+//Muchas veces necesitarás cambiar ciertas variables(parámetros) por lo que siempre debes
+//Hacer que tus funciones sirvan con cualquier tipo de parámetros.
+
+//1.Permite que la funcion retorne el resultado de la suma de ambos numeros.
+function suma(a, b) {
+  //tu codigo debajo
+  
+}
+
+suma(5, 10);
+
+//2.Permite que la funcion retorne el resultado de la resta de ambos numeros.
+function resta() {
+  //tu codigo debajo
+
+}
+
+resta(20, 12);
+
+//3.Permite que la funcion retorne el resultado de la multiplicacion de ambos numeros.
+function multiplicacion() {
+  //tu codigo debajo
+
+}
+
+multiplicacion(8, 3);
+
+//4.Permite que la funcion retorne el resultado de la division de ambos numeros.
+function division() {
+  //tu codigo debajo
+
+}
+
+division(20, 6);
+
+//5.Te llegarán dos frases. Concatenalas, pero revisalas bien para que el retorno tenga sentido.
+function frase() {
+  //tu codigo debajo
+
+}
+
+frase("luego limpiaré los platos","Preparé mi comida");
+
+//6.Retorna true si los dos valores son iguales, o retorna false de lo contrario
+function comparacion() {
+  //tu codigo debajo
+
+} 
+
+comparacion(8, 12);
+
+//7.Suma la cantidad de caracteres de dos nombres que te llegarán por parámetro
+function sumaCaracteres() {
+  //tu codigo debajo
+
+} 
+
+sumaCaracteres("Ramon", "Antonella");
+
+
+//8.De nuevo te daré dos nombres. Retorna el nombre con el length mas alto. 
+// Si los dos nombres tienen la misma longitud retorna un string que diga: "Tiene el mismo length " + {agrega aqui el length}
+//Recuerda que los parametros pueden 
+//cambiar pero tu codigo debe servir igual de bien
+
+function nombreLargo() {
+  //tu codigo debajo
+
+} 
+
+nombreLargo("Joaquin", "Natanael");
+
+//9.Saluda a una persona por su nombre
+function sumaCaracteres(nombre) {
+  //tu codigo debajo
+  return "que tengas un buen día"
+} 
+
+sumaCaracteres("Andres");
+
+//10.Te daré una lista de numeros. Crearás una variable donde vas a duplicar el array, y a cada elemento del array
+// le vas a agregar este simbolo ° ejemplo: 1°, 2°, 3°, 4°, 5°, luego retorna esa copia editada del array
+function editarArray(array) {
+  //tu codigo debajo
+  
+} 
+
+sumaCaracteres([1, 2, 3, 4, 5]);
